0b485df714db6f7422620845354e621059f36d17
Soyoung <ksyoung3424@gmail.com>
2023-05-26 01:24:22 +0000

    Create web server with Express

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express(); // creates an instance of the express application
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => { // client sends a Get request to /
+  res.send("Hello!");  // server sends the response back to the client
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
