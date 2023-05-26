2cd9516af5484184c57a0e1c55180fbb0141be37
Soyoung <ksyoung3424@gmail.com>
2023-05-26 01:35:15 +0000

    Add /hello endpoint with HTML code response

--- express_server.js
+++ express_server.js
@@ -15,6 +15,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n"); //server sends the response using HTML code to the client
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
