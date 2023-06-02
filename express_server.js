bbc273e51bf6bc417538a1b425d07d5539962a91
Soyoung <ksyoung3424@gmail.com>
2023-06-02 02:21:51 +0000

    Add a GET route for /register which renders the registration template

--- express_server.js
+++ express_server.js
@@ -101,6 +101,14 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+// Register
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_register", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
